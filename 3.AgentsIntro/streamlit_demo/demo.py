20d1289bc21e3935cb52ffcfb1f24da64a5ac8e3
Anurag <anurg@yahoo.com>
2026-08-20 06:35:58 +0000

    ...

---
+++ 3.AgentsIntro/streamlit_demo/demo.py
@@ -0,0 +1,9 @@
+import streamlit as st
+st.set_page_config()
+
+st.title("Streamlit Basics")
+st.caption("Tiny Tour of the Streamlit")
+st.write("Hello Streamlit..")
+
+st.markdown("hello ")
+st.button("Hello")
